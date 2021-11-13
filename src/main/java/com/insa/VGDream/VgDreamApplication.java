586757e67ef9e92bbc1234f91e56823dd7e1f741
Massar <massarabbas62@gmail;com>
2021-11-13 09:15:33 +0100

    Initiating java project

---
+++ src/main/java/com/insa/VGDream/VgDreamApplication.java
@@ -0,0 +1,13 @@
+package com.insa.VGDream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VgDreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VgDreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/insa/VGDream/VgDreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insa.VGDream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VgDreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
