d6171499cf8c099ee20a03090632090a6eb76fa1
Maxime Frémeaux <maxime.fremeaux@charlie-solutions.com>
2021-12-16 00:04:24 +0100

    add header + default route + css modifs

--- src/main/java/com/insa/VGDream/VgDreamApplication.java
+++ src/main/java/com/insa/VGDream/VgDreamApplication.java
@@ -2,9 +2,17 @@ package com.insa.VGDream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
 
+@RestController
 @SpringBootApplication
 public class VgDreamApplication {
+	@RequestMapping ( "/")
+	public RedirectView redirectView(){
+		return new RedirectView("/home.html");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VgDreamApplication.class, args);
